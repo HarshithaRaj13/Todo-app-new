5eb48e660039ad1fe0b8fefc9f9ec32302ad2c5f
HarshithaRaj13 <harshitharajshekar13@gmail.com>
2024-06-11 10:35:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { DeleteTodo } from "./components/DeleteTodo";
+
+function App() {
+  return (
+    <div className="App">
+      <DeleteTodo />
+    </div>
+  );
+}
+
+export default App;
